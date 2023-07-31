35b29f5aa8ba042f0676d96c3971fdd9b81ce79d
whitecolinpai <colinpkl@gmail.com>
2023-07-31 12:01:07 +0800

    update

--- resources/views/membercenterpage/myaccount.blade.php
+++ resources/views/membercenterpage/myaccount.blade.php
@@ -1,1 +1,10 @@
-2
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <x-membercenter.title />
+        <x-membercenter.member-center-inner>
+            <x-membercenter.navbar />
+        </x-membercenter.member-center-inner>
+    </div>
+@endsection
